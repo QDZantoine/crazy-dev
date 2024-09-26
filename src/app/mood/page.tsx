c8a2ad37d45820fc5a32f48dc70951273727461a
zakhiel50 <lucrigaud50@gmail.com>
2024-09-26 11:22:37 +0200

    -Add card on mood icons

--- src/app/mood/page.tsx
+++ src/app/mood/page.tsx
@@ -1,7 +1,14 @@
 'use client';
 import Navbar from '@/components/ui/navbarTailwind'
 import { Angry, Frown, Annoyed, Laugh, Meh, Smile, Heart } from 'lucide-react';
-
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card"
 
 
 export default function Articles() {
@@ -10,15 +17,30 @@ export default function Articles() {
     return (
         <div>
             <Navbar></Navbar>
-            <section style={{display:'flex', justifyContent: 'space-around'}}>
+            <section style={{ display: 'flex', justifyContent: 'space-around', alignItems: 'center', minHeight: 400, width: '80%', marginLeft: 'auto', marginRight: 'auto' }}>
+                <Card style={{ padding: 20, backgroundColor: 'red' }}>
+                    <Angry size={100} />
+                </Card>
 
-                <Angry size={100} color='red'/>
-                <Annoyed size={100} color='yellow'/>
-                <Frown size={100} color='orange'/>
-                <Laugh size={100} color='green'/>
-                <Meh size={100} color='salmon'/>
-                <Smile size={100} color='lightgreen'/>
-                <Heart size={100} color='coral'/>
+                <Card style={{ padding: 20, backgroundColor: 'orange' }}>
+                    <Frown size={100} />
+                </Card>
+
+            
+                <Card style={{ padding: 20, backgroundColor: 'green' }}>
+                <Laugh size={100}/>
+                </Card>
+                <Card style={{ padding: 20, backgroundColor: 'salmon' }}>
+                    <Meh size={100} />
+                </Card>
+
+                <Card style={{ padding: 20, backgroundColor: 'lightgreen' }}>
+                    <Smile size={100} />
+                </Card>
+
+                <Card style={{ padding: 20, backgroundColor: 'coral' }}>
+                    <Heart size={100} />
+                </Card>
 
             </section>
 
