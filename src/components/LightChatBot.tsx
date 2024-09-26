a5127ea6e9802f06e7cebdd2922644fd5b6dd86d
Quendez antoine <antoine.quendez@gmail.com>
2024-09-26 17:28:24 +0200

    feat: add chatgpt connexion and prompt

--- src/components/LightChatBot.tsx
+++ src/components/LightChatBot.tsx
@@ -2,6 +2,7 @@
 import { useEffect, useState } from 'react';
 import { Bot, Send } from 'lucide-react';
 import { useTheme } from 'next-themes';
+import OpenAI from 'openai';
 
 type Message = {
   sender: string;
@@ -10,44 +11,40 @@ type Message = {
 
 const LightChatBot = () => {
   const [messages, setMessages] = useState<Message[]>([]);
-  const [input, setInput] = useState('');
+  const [inputValue, setInputValue] = useState('');
   const { theme } = useTheme();
   const [isDarkMode, setIsDarkMode] = useState(false);
-
-  useEffect(() => {
-    setIsDarkMode(theme === 'dark');
-  }, [theme]);
-
-  if (isDarkMode) return null;
-
-  const handleSendMessage = async () => {
-    if (input.trim() === '') return;
-
-    const newMessage = { sender: 'user', text: input };
-    setMessages([...messages, newMessage]);
-    setInput('');
-
-    try {
-      const response = await fetch('/api/chatgpt', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
+  const [responseAI, setResponseAI] = useState<string | undefined | null>('');
+  const [loader, setLoader] = useState<boolean>(false);
+  const openai = new OpenAI({
+    apiKey: process.env.NEXT_PUBLIC_OPENAI_API_KEY,
+    dangerouslyAllowBrowser: true,
+  });
+  async function main() {
+    setLoader(true);
+    const completion = await openai.chat.completions.create({
+      messages: [
+        {
+          role: 'system',
+          content: `${inputValue} Je veux ma réponse en français et au maximum de 400 caractères`,
         },
-        body: JSON.stringify({ message: input }),
-      });
+      ],
+      model: 'gpt-4o',
+    });
 
-      const data = await response.json();
-      const botMessage = { sender: 'bot', text: data.response };
-      setMessages((prev) => [...prev, botMessage]);
-    } catch (error) {
-      console.error('Error fetching ChatGPT response:', error);
-    }
-  };
+    setResponseAI(completion.choices[0].message.content);
+    console.log(completion.choices[0]);
+  }
 
   return (
     <div className="p-4 rounded-lg shadow-lg bg-white w-96">
       <div className="h-64 overflow-y-auto mb-4">
-      <Bot size={40} className="text-gray-500 dark:text-gray-400 mr-2" />
+        <Bot size={40} className="text-gray-500 dark:text-gray-400 mr-2" />
+        {!responseAI && loader ? (
+          <div style={{ width: 50, height: 50, backgroundColor: 'blue' }}></div>
+        ) : (
+          <p>{responseAI}</p>
+        )}
         {messages.map((message, index) => (
           <div
             key={index}
@@ -71,13 +68,13 @@ const LightChatBot = () => {
         <input
           className="flex-grow p-2 border border-gray-300 rounded-l-lg"
           type="text"
-          value={input}
-          onChange={(e) => setInput(e.target.value)}
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
           placeholder="Type your message..."
         />
         <button
           className="p-2 bg-violet-400 text-white rounded-r-lg"
-          onClick={handleSendMessage}
+          onClick={main}
         >
           <Send size={16} />
         </button>
